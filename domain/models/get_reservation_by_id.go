7c2ad8232c2e173b2d913ac13e9a9ca249751ad4
Jordany <j2d6.pro@gmail.com>
2025-11-16 08:50:06 +0300

    add custome time

--- domain/models/get_reservation_by_id.go
+++ domain/models/get_reservation_by_id.go
@@ -1,15 +1,17 @@
 package models
 
 import (
-    "time"
-    "github.com/google/uuid"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type ReservationCompleteT struct {
     ReservationID     uuid.UUID          `json:"reservation_id"`
     Email             string             `json:"email"`
     ReferencePaiement string             `json:"reference_paiement"`
-    DateReservation   time.Time          `json:"date_reservation"`
+    DateReservation   CustomTime         `json:"date_reservation"`  // Changé ici
     Etat              string             `json:"etat"`
     EtatCode          string             `json:"etat_code"`
     Evenement         EvenementReservationT `json:"evenement"`
@@ -19,13 +21,13 @@ type ReservationCompleteT struct {
 }
 
 type EvenementReservationT struct {
-    ID            uuid.UUID        `json:"id"`
-    Titre         string           `json:"titre"`
-    Description   string           `json:"description"`
-    DateDebut     time.Time        `json:"date_debut"`
-    DateFin       time.Time        `json:"date_fin"`
-    TypeEvenement TypeEvenement    `json:"type_evenement"`
-    Lieu          Lieu             `json:"lieu"`
+    ID            uuid.UUID     `json:"id"`
+    Titre         string        `json:"titre"`
+    Description   string        `json:"description"`
+    DateDebut     CustomTime    `json:"date_debut"`  // Changé ici
+    DateFin       CustomTime    `json:"date_fin"`    // Changé ici
+    TypeEvenement TypeEvenement `json:"type_evenement"`
+    Lieu          Lieu          `json:"lieu"`
 }
 
 type PlaceReservationT struct {
@@ -40,4 +42,31 @@ type TarifPlaceT struct {
     ID        uuid.UUID   `json:"id"`
     Prix      float64     `json:"prix"`
     TypePlace TypePlace   `json:"type_place"`
+}
+
+
+type CustomTime time.Time
+
+const customTimeFormat = "2006-01-02T15:04:05"
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) error {
+    s := strings.Trim(string(b), "\"")
+    if s == "null" {
+        *ct = CustomTime(time.Time{})
+        return nil
+    }
+    t, err := time.Parse(customTimeFormat, s)
+    if err != nil {
+        return err
+    }
+    *ct = CustomTime(t)
+    return nil
+}
+
+func (ct CustomTime) MarshalJSON() ([]byte, error) {
+    return []byte(`"` + time.Time(ct).Format(customTimeFormat) + `"`), nil
+}
+
+func (ct CustomTime) Time() time.Time {
+    return time.Time(ct)
 }
